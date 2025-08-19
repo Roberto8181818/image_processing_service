c26ffc72414aa2878d2f238cec96708c15ecb095
Roberto <robertopadillapadilla9@gmail.com>
2025-08-19 00:58:18 -0500

    feat(app): serve uploaded images via static /uploads route

--- src/app.js
+++ src/app.js
@@ -2,10 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const authRoutes = require('./routes/authRoutes');
+const imageRoutes = require('./routes/imageRoutes');
+const authMiddleware = require('./middlewares/auth')
 
 app.use(express.json());
 
 app.use('/auth', authRoutes);
+
+app.use('/images', imageRoutes);
+
+app.use("/uploads", express.static("uploads"));
 
 app.get('/', (req, res) => {
   res.json({ message: 'API OK' });
