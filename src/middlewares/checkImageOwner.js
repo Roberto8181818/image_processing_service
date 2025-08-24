4b7b1a285363c4386588488888dac300f2b68929
Roberto <robertopadillapadilla9@gmail.com>
2025-08-23 23:46:50 -0500

    feat(middleware): add checkImageOwner to restrict image modifications to the owner

---
+++ src/middlewares/checkImageOwner.js
@@ -0,0 +1,24 @@
+const { Image } = require("../models");
+
+const checkImageOwner = async (req, res, next) => {
+  try {
+    const imageId = req.params.id;
+    const image = await Image.findByPk(imageId);
+
+    if (!image) {
+      return res.status(404).json({ message: "Imagen no encontrada" });
+    }
+
+    if (image.user_id !== req.user.id) {
+      return res.status(403).json({ message: "No tienes permisos para editar esta imagen" });
+    }
+
+    req.image = image;
+    next();
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Error verificando permisos" });
+  }
+};
+
+module.exports = checkImageOwner;
