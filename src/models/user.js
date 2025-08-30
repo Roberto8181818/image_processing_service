bebc1bc8eab1da9c80d7bb00aa2b7a50609540af
Roberto <robertopadillapadilla9@gmail.com>
2025-08-30 11:01:25 -0500

    feat(models): add User-Image relations and generateToken method to User

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,7 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
+const jwt = require("jsonwebtoken");
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -10,15 +10,29 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      User.hasMany(models.Image, {
+        foreignKey: "user_id",
+        as: "Images",
+        onDelete: "CASCADE",
+      });
+    }
+    generateToken() {
+      return jwt.sign(
+        { id: this.id, username: this.username },
+        process.env.JWT_SECRET || "secret",
+        { expiresIn: "1h" }
+      );
     }
   }
-  User.init({
-    username: DataTypes.STRING,
-    password_hash: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  User.init(
+    {
+      username: DataTypes.STRING,
+      password_hash: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
   return User;
 };
