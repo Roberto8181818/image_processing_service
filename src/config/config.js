208e63f89ce7a254c9994bf4e2db94077300886e
Roberto <robertopadillapadilla9@gmail.com>
2025-08-30 11:02:07 -0500

    chore(config): update config.js to use environment variables for all environments

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,30 @@
-require('dotenv').config();
-
+require("dotenv").config();
 module.exports = {
   development: {
-    url: process.env.DATABASE_URL,
-    dialect: 'postgres',
+    username: process.env.DB_DEV_USER,
+    password: process.env.DB_DEV_PASSWORD,
+    database: process.env.DB_DEV_NAME,
+    host: process.env.DB_DEV_HOST,
+    port: process.env.DB_DEV_PORT,
+    dialect: "postgres",
+    logging: false,
+  },
+  test: {
+    username: process.env.DB_TEST_USER,
+    password: process.env.DB_TEST_PASSWORD,
+    database: process.env.DB_TEST_NAME,
+    host: process.env.DB_TEST_HOST,
+    port: process.env.DB_TEST_PORT,
+    dialect: "postgres",
+    logging: false,
+  },
+  production: {
+    username: process.env.DB_PROD_USER,
+    password: process.env.DB_PROD_PASSWORD,
+    database: process.env.DB_PROD_NAME,
+    host: process.env.DB_PROD_HOST,
+    port: process.env.DB_PROD_PORT,
+    dialect: "postgres",
+    logging: false,
   },
 };
